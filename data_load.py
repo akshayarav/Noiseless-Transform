dfc648a0f7af73310b1a3c1db37ca354460f83fb
Ram <rbpeddu@gmail.com>
2025-04-26 15:00:05 -0400

    Data loader

---
+++ data_load.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+
+IMG_SIZE   = 128
+BATCH_SIZE = 64
+T_STEPS    = 300        
+DEVICE     = "cuda" if torch.cuda.is_available() else "cpu"
+
+transform = transforms.Compose([
+    transforms.Resize(IMG_SIZE),
+    transforms.CenterCrop(IMG_SIZE),
+    transforms.ToTensor(),               # [0,1]
+    transforms.Normalize([0.5]*3, [0.5]*3)  # scale to [-1,1]
+])
+
+celeba = datasets.CelebA(root="data", split="validation", download=True, transform=transform)
+loader = DataLoader(celeba, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+print(len(loader))
