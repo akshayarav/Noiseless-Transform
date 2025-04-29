1acfdf6977618456d29d60759e86637f9974a46b
Nathan Palamuttam <npalamuttam04@gmail.com>
2025-04-29 14:48:30 -0400

    change to gaussian blur and switch to research paper unet

--- main.py
+++ main.py
@@ -1,102 +1,85 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+import os
+from kernel import swirl_function, blur_function
+from model import Unet
+import torch.nn as nn
 
+l1 = nn.L1Loss()
+BATCH_SIZE = 32
+LR = 2e-4
+EPOCHS = 20
+IMG_SIZE = 64
 T_STEPS = 100
-
-class TimeEmbedding(nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.lin = nn.Linear(1, dim)
-    def forward(self, t):
-        return torch.relu(self.lin(t.unsqueeze(-1)))
-
-class SimpleUNet(nn.Module):
-    def __init__(self, in_channels=3, time_dim=32):
-        super().__init__()
-        self.time_emb = TimeEmbedding(time_dim)
-        self.down1 = nn.Conv2d(in_channels, 64, 4, 2, 1)
-        self.down2 = nn.Conv2d(64, 128, 4, 2, 1)   
-        self.down3 = nn.Conv2d(128, 256, 4, 2, 1) 
-
-        self.time_mlp = nn.Linear(time_dim, 256)  
-        self.up0 = nn.ConvTranspose2d(256, 128, 4, 2, 1) 
-        self.up1 = nn.ConvTranspose2d(256, 64, 4, 2, 1) 
-        self.conv1x1 = nn.Conv2d(128, 64, kernel_size=1) 
-        self.up2 = nn.ConvTranspose2d(64, in_channels, 4, 2, 1) 
-
-    def forward(self, x, s):
-        t_emb = self.time_emb(s.float() / T_STEPS)  
-        t_emb = self.time_mlp(t_emb)
-        h1 = torch.relu(self.down1(x))
-        h2 = torch.relu(self.down2(h1)) 
-        h3 = torch.relu(self.down3(h2) + t_emb.view(-1, t_emb.shape[-1], 1, 1)) 
-        h = torch.relu(self.up0(h3)) 
-        h = torch.cat([h, h2], dim=1) 
-        h = torch.relu(self.up1(h)) 
-        h = torch.cat([h, h1], dim=1) 
-        h = self.conv1x1(h)
-        out = self.up2(h)
-
-        return out
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+OUTPUT_FOLDER = "generated_images"
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 
+#change this transform back - testing on CIFAR-10 currently
+transform = transforms.Compose([
+    #transforms.CenterCrop(140),
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.ToTensor(),
+])
 
-# import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
+dataset = torchvision.datasets.CIFAR10(
+    root="./small_data",
+    train=True,
+    download=True,
+    transform=transform
+)
+loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
-# T_STEPS = 100
-# class TimeEmbedding(nn.Module):
-#     def __init__(self, dim):
-#         super().__init__()
-#         self.lin = nn.Linear(1, dim)
-#     def forward(self, t):
-#         return torch.relu(self.lin(t.unsqueeze(-1)))
-
-# class SimpleUNet(nn.Module):
-#     def __init__(self, in_channels=3, time_dim=32):
-#         super().__init__()
-#         self.time_emb = TimeEmbedding(time_dim)
-        
-#         self.down1 = nn.Conv2d(in_channels, 64, 4, 2, 1)
-#         self.down2 = nn.Conv2d(64, 128, 4, 2, 1)
-#         self.up1   = nn.ConvTranspose2d(128, 64, 4, 2, 1)
-#         self.up2   = nn.ConvTranspose2d(64, in_channels, 4, 2, 1)
-#         self.conv1x1 = nn.Conv2d(128, 64, kernel_size=1)
-#         self.time_mlp = nn.Linear(time_dim, 128)
-
-#     def forward(self, x, s):
-#       t_emb = self.time_emb(s.float() / T_STEPS)  
-#       t_emb = self.time_mlp(t_emb)
-#       h1 = torch.relu(self.down1(x))
-#       h2 = torch.relu(self.down2(h1) + t_emb.view(-1, t_emb.shape[-1], 1, 1))
-
-#       h = torch.relu(self.up1(h2))
-#       h = torch.cat([h, h1], dim=1)
-#       h = self.conv1x1(h)
-
-#       return self.up2(h)
-
-    
+model = Unet(32).to(DEVICE)
+optimizer = optim.Adam(model.parameters(), lr=LR)
 
 
-# from tqdm import tqdm
-# import random
-# from kernel import swirl_function
-# import torch.nn as nn
+def train():
+    for epoch in range(EPOCHS):
+        model.train()
+        for i, (x0, _) in enumerate(loader):
+            x0 = x0.to(DEVICE)
+            s = torch.randint(1, T_STEPS+1, (x0.size(0),), device=DEVICE)
+            xs = torch.stack([blur_function(x.unsqueeze(0), step.item())[0] for x, step in zip(x0, s)], dim=0)
 
-# l1 = nn.L1Loss()
-# def training_loop(model, loader, optimizer, device):
-#     for epoch in range(20):
-#         progressbar = tqdm(loader)
-#         for x0, _ in progressbar:
-#             x0 = x0.to(device)
-#             s = random.randint(1, T_STEPS)
-#             xs = swirl_function(x0, s)                    
-#             x_pred = model(xs, torch.tensor([s]*x0.size(0), device=device))
-#             loss = l1(x_pred, x0)            
-#             optimizer.zero_grad()
-#             loss.backward()
-#             optimizer.step()
-#             progressbar.set_description(f"Epoch {epoch} Loss {loss.item():.4f}")
+            x_pred = model(xs, s)
+            loss = l1(x_pred, x0)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if i % 100 == 0:
+                print(f"Epoch [{epoch}/{EPOCHS}] Batch [{i}/{len(loader)}] Loss: {loss.item():.4f}")
+
+        save_generated_images(epoch, loader)
+
+def save_generated_images(epoch, data_loader):
+    model.eval()
+    with torch.no_grad():
+        x0, _ = next(iter(data_loader))
+        x0 = x0.to(DEVICE)
+        s_full = torch.full((x0.size(0),), T_STEPS, device=DEVICE)
+        x = torch.stack([blur_function(img.unsqueeze(0), step.item())[0] for img, step in zip(x0, s_full)], dim=0)
+
+        degraded_sample = x.clone()
+        for s in reversed(range(1, T_STEPS + 1)):
+            s_tensor = torch.full((x.size(0),), s, device=DEVICE)
+
+            x0_hat = model(x, s_tensor)
+            D_s = torch.stack([blur_function(img.unsqueeze(0), s)[0] for img in x0_hat], dim=0)
+            D_s_minus_1 = torch.stack([blur_function(img.unsqueeze(0), s-1)[0] for img in x0_hat], dim=0)
+            x = x - D_s + D_s_minus_1
+        save_image(x.cpu(), os.path.join(OUTPUT_FOLDER, f"sample_epoch_{epoch}.png"), nrow=4, normalize=True)
+
+
+
+
+if __name__ == "__main__":
+    train()
